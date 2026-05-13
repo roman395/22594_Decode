02bf7b0abec492139417b020347baab7ef43c5d3
roman395 <goruskinroma@gmail.com>
2026-05-13 07:34:43 +0300

    Module.java fix

--- TeamCode/src/main/java/Module.java
+++ TeamCode/src/main/java/Module.java
@@ -3,7 +3,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public abstract class Module {
-    public Module(LinearOpMode linearOpMode){}
+    public Module(){}
     public void update(){}
     public void addTelemetry(Telemetry telemetry){}
 }
