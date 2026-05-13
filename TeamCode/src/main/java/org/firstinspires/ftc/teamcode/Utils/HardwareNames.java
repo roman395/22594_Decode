031c3b1ecf03c55090c13041acd7af57819a997e
roman395 <goruskinroma@gmail.com>
2026-05-13 15:36:42 +0300

    update motor maker and hardware names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/HardwareNames.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/HardwareNames.java
@@ -1,6 +1,21 @@
 package org.firstinspires.ftc.teamcode.Utils;
 
 public class HardwareNames {
-  public static final String
-  FEEDER_MOTOR = "";
+  public static final String DriveFLM = "FLM",
+      DriveFRM = "FRM",
+      DriveRLM = "RLM",
+      DriveRRM = "RRM",
+      StrafeEncoder = "RRM",
+      ForwardEncoder = "FRM",
+      Logitech = "camera",
+      IntakeMotor = "intake",
+      FeederMotor = "feeder",
+      TurretServo1 = "tServo1",
+      TurretServo2 = "tServo2",
+      TurretServoEncoder1 = "tServoEn1",
+      TurretServoEncoder2 = "tServoEn2",
+      RevDistanceSensor = "sensor",
+      ShootServo = "shootS",
+      ShootLeft = "shootL",
+      ShootRight = "shootR";
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/MotorMaker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/MotorMaker.java
@@ -14,34 +14,40 @@ public class MotorMaker {
   private DcMotorSimple.Direction direction = DcMotorSimple.Direction.FORWARD;
   private LinearOpMode linearOpMode;
   
-  public MotorMaker(String motorName, LinearOpMode linearOpMode){
+  public MotorMaker(String motorName, LinearOpMode linearOpMode) {
     this.motorName = motorName;
     this.linearOpMode = linearOpMode;
   }
-  public MotorMaker setZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior){
+  
+  public MotorMaker setZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
     this.zeroPowerBehavior = zeroPowerBehavior;
     return this;
   }
-  public MotorMaker setRunMode(DcMotor.RunMode runMode){
+  
+  public MotorMaker setRunMode(DcMotor.RunMode runMode) {
     this.runMode = runMode;
     return this;
   }
-  public MotorMaker setDirection(DcMotorSimple.Direction direction){
+  
+  public MotorMaker setDirection(DcMotorSimple.Direction direction) {
     this.direction = direction;
     return this;
   }
-  public DcMotor build(){
+  
+  public DcMotor build() {
     motor = linearOpMode.hardwareMap.get(DcMotor.class, motorName);
     motor.setMode(runMode);
     motor.setZeroPowerBehavior(zeroPowerBehavior);
     motor.setDirection(direction);
     return motor;
   }
-  public DcMotor buildEx(){
+  
+  public DcMotor buildEx() {
     motorEx = linearOpMode.hardwareMap.get(DcMotorEx.class, motorName);
     motorEx.setMode(runMode);
     motorEx.setZeroPowerBehavior(zeroPowerBehavior);
     motorEx.setDirection(direction);
     return motorEx;
   }
+  
 }
