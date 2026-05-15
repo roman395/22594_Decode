28dd000472fdd556d319093f897b606e0cee3ddb
roman395 <goruskinroma@gmail.com>
2026-05-15 08:27:37 +0300

    Add basic logic for Shooter

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configs/ShooterConfig.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Configs;
+
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+public class ShooterConfig {
+  public static PIDFCoefficients COEFFICIENTS = new PIDFCoefficients(0.003, 0, 0, 0.00036);
+  public static double INACCURACY = 35;
+  
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/Shooter.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.Modules;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Configs.HardwareNames;
+import org.firstinspires.ftc.teamcode.Configs.ShooterConfig;
+import org.firstinspires.ftc.teamcode.StateMachine.IStateMachineCaller;
+import org.firstinspires.ftc.teamcode.StateMachine.RobotStates;
+import org.firstinspires.ftc.teamcode.Utils.MotorMaker;
+import org.firstinspires.ftc.teamcode.Utils.PID;
+
+public class Shooter extends Module implements IStateMachineCaller {
+  private DcMotorEx leftMotor, rightMotor;
+  private double currentVelocity, targetVelocity;
+  private PID pidRegulator = new PID(ShooterConfig.COEFFICIENTS);
+  
+  public Shooter(LinearOpMode linearOpMode) {
+    leftMotor = new MotorMaker(HardwareNames.ShootLeft, linearOpMode)
+        .setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+        .buildEx();
+    //Set as master for pid calculation
+    rightMotor = new MotorMaker(HardwareNames.ShootRight, linearOpMode)
+        .setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+        .buildEx();
+  }
+  
+  @Override
+  public void update() {
+    currentVelocity = rightMotor.getVelocity();
+  }
+  
+  @Override
+  public void addTelemetry(Telemetry telemetry) {
+    telemetry.addData("Current velocity: ", currentVelocity);
+    telemetry.addData("Target velocity: ", targetVelocity);
+  }
+  
+  @Override
+  public void onCall(RobotStates state) {
+    if (state == RobotStates.SPOOLING || state == RobotStates.SHOOTING) {
+      double power = pidRegulator.calculateVelocity(currentVelocity - targetVelocity, targetVelocity);
+      leftMotor.setPower(power);
+      rightMotor.setPower(power);
+    }
+  }
+  
+  @Override
+  public RobotStates requestState(RobotStates currentState) {
+    if (currentState == RobotStates.SPOOLING && Math.abs(currentVelocity - targetVelocity) < ShooterConfig.INACCURACY)
+      return RobotStates.READY_TO_SHOOT;
+    return null;
+  }
+  
+}
