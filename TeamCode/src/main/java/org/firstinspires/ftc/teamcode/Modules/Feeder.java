b1625ae25387aadde9d639d0e71ba59f9910e3ea
roman395 <goruskinroma@gmail.com>
2026-05-15 07:51:24 +0300

    formatting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/Feeder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modules/Feeder.java
@@ -47,7 +47,7 @@ public class Feeder extends Module implements IStateMachineCaller {
   
   @Override
   public RobotStates requestState(RobotStates currentState) {
-    if(currentSensorDistance < FeederConfig.distanceWhenEmpty && currentState == RobotStates.INTAKING)
+    if (currentSensorDistance < FeederConfig.distanceWhenEmpty && currentState == RobotStates.INTAKING)
       return RobotStates.BALL_IN_FEEDER;
     return null;
   }
