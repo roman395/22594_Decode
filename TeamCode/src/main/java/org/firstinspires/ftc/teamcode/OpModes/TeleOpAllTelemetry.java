e76d43288897d48d9588330e518f5fea61753515
roman395 <goruskinroma@gmail.com>
2026-05-30 16:49:02 +0300

    Upgrade telemetry and add TeleOpAllTelemetry

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpAllTelemetry.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(group = "OpModes", name = "RED_TeleOp")
+public class TeleOpAllTelemetry extends TeleOpBase {
+  @Override
+  public int getAllianceAprilTagID() {
+    return 24;
+  }
+  
+  @Override
+  public void addTelemetry(){
+    machine.addTelemetry(telemetry);
+    shooter.addTelemetry(telemetry);
+    intake.addTelemetry(telemetry);
+    feeder.addTelemetry(telemetry);
+    turret.addTelemetry(telemetry);
+    drivetrain.addTelemetry(telemetry);
+  }
+  
+  @Override
+  public Pose getGoalPose() {
+    return null;
+  }
+  
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpBase.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.OpModes;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Camera.LimeLightCamera;
 import org.firstinspires.ftc.teamcode.Modules.Drivetrain;
 import org.firstinspires.ftc.teamcode.Modules.Feeder;
@@ -22,6 +23,7 @@ public abstract class TeleOpBase extends LinearOpMode {
   
   @Override
   public void runOpMode() throws InterruptedException {
+    
     drivetrain = new Drivetrain(this);
     machine = new StateMachine(this);
     intake = new Intake(this);
@@ -29,19 +31,27 @@ public abstract class TeleOpBase extends LinearOpMode {
     limeLightCamera = new LimeLightCamera(getAllianceAprilTagID(), this);
     turret = new Turret(this, limeLightCamera, getGoalPose());
     shooter = new Shooter(this, limeLightCamera);
+    
     machine.addListener(intake);
     machine.addListener(feeder);
     machine.addListener(turret);
     machine.addListener(shooter);
+    
     waitForStart();
+    
     while (opModeIsActive()) {
       drivetrain.update();
       machine.update();
+      addTelemetry();
+      telemetry.update();
     }
   }
   
   public abstract int getAllianceAprilTagID();
   
   public abstract Pose getGoalPose();
+  public void addTelemetry(){
+    machine.addTelemetry(telemetry);
+  }
   
 }
